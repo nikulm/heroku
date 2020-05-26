af51255ad49b622674a61c9319d60b2e931ff468
user <user@users-iMac.local>
2020-05-26 11:20:20 +0800

    make it better

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const router = express.Router();
+const axios = require("axios");
+
+app.use("/", router);
+var url = "http://mock-api.com/6KL5q6zk.mock/newversion";
+
+router.get("/newversion", (req, res) => {
+  axios
+    .get(url)
+    .then((response) => {
+      res.send(response.data);
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(400).send("Error");
+    });
+});
+
+router.get("/", (req, res) => res.send("Hello!"));
+
+const hostname = "0.0.0.0";
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Serving running at http://${hostname}:${port}/`);
+});
